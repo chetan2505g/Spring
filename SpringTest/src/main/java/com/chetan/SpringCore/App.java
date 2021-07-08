047cdd6c934d0814d8fb71b99352bce2feb7a5fe
chetan2505g <62828793+chetan2505g@users.noreply.github.com>
2021-07-08 17:43:31 +0530

    springCore package

---
+++ SpringTest/src/main/java/com/chetan/SpringCore/App.java
@@ -0,0 +1,15 @@
+package com.chetan.SpringCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World! Chetan123456890" );
+
+    }
+}
